4a752e19b615e317c0e79f2f954be7ed612c6694
Thomas Levine <_@thomaslevine.com>
2016-03-11 08:51:27 +0000

    file selectors

---
+++ R/file-selectors.r
@@ -0,0 +1,7 @@
+select.files <- function(directory)
+  list.files(directory, recursive=TRUE)
+
+select.dirs <- function(directory)
+  list.dirs(directory, recursive=TRUE)
+
+# list.dirs(directory, recursive=FALSE)
