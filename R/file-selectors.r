e66cb3ed0c1a36190ce3bcc911df4f77f52eed5d
Thomas Levine <_@thomaslevine.com>
2016-03-11 09:23:08 +0000

    woo

--- R/file-selectors.r
+++ R/file-selectors.r
@@ -12,13 +12,17 @@ select.git.repositories <- function(directory) {
                      select.git.repositories), use.names=FALSE)
 }
 
+.has.no.subdirectory <- function(directory)
+  0 == length(list.dirs(directory, full.names=FALSE, recursive=FALSE))
+
 REPOSITORY.FILES <- c('^.git$', '^DESCRIPTION$', '^README.*$')
 select.repositories <- function(directory) {
   directory <- sub('/*$', '', directory)
-  files <- list.dirs(directory, full.names=FALSE, recursive=FALSE)
+  files <- list.files(directory, full.names=FALSE, recursive=FALSE,
+                      all.files=TRUE)
   matches <- mapply(grepl, rep(REPOSITORY.FILES, each=length(files)),
                     rep(files, length(REPOSITORY.FILES)))
-  if (any(matches)) directory
+  if (any(matches) || .has.no.subdirectory(directory)) directory
   else unlist(sapply(list.dirs(directory, recursive=FALSE),
-                     select.git.repositories), use.names=FALSE)
+                     select.repositories), use.names=FALSE)
 }
